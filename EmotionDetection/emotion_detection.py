3febafc554d580e667a562768339efab7194751d
Dmitrii Skvortsov <618skvortsov@gmail.com>
2023-12-06 21:18:38 -0500

    Lab finished

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,6 +7,15 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)
     formatted_response = json.loads(response.text)
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
     res = {
         'anger': formatted_response["emotionPredictions"][0]["emotion"]["anger"],
         'disgust': formatted_response["emotionPredictions"][0]["emotion"]["disgust"],
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""Server for Emotion Detection App"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,12 +6,21 @@ app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
 def sent_detector():
+    """Detector route response function"""
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+    if response["dominant_emotion"] is None:
+        return "Invalid text! Please try again!"
+    return (f"""For the given statement, the system response is
+        'anger': {response['anger']}, 'disgust': {response['disgust']}, 
+        'fear': {response['fear']}, 'joy': {response['joy']} and 
+        'sadness': {response['sadness']}. The dominant emotion 
+        is {response['dominant_emotion']}."""
+    )
 
 @app.route("/")
 def render_index_page():
+    """Index route response function"""
     return render_template('index.html')
 
 if __name__ == "__main__":
